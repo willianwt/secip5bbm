411837c4fc274b278708fc3f6fcc9402b42aa8ac
Willian Taiguara <willianwt@gmail.com>
2020-03-31 22:11:24 -0300

    erro pt

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -62,7 +62,7 @@ export default function Home() {
           </ul>
           <p>Este serviço foi feito com intuito de ajudar aqueles que, por qualquer motivo, tenham alguma dificuldade em procedimentos da seção. </p>
           <p>Críticas construtivas e sugestões são sempre bem vindas.</p>
-          <p>Em caso de problemas, este site poderá ser tirado do ar sem aviso prévio.</p>
+          <p>Em caso de problemas, este site poderá ser retirado do ar sem aviso prévio.</p>
           <p>Você pode começar a utilizar clicando nos botões na barra de navegação acima. </p>
           <p className="text-right">- Cabo Willian</p>
         </div>
