5c6d80196d0342ea5470102a3f4fb18200fdc890
Willian Taiguara <willianwt@gmail.com>
2020-07-15 22:39:48 -0300

    cancelada alteração anterior

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 
 import store from './store';
 import history from './services/history';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import GlobalStyle from './styles/GlobalStyles';
-import Header from './components/Header';
-import Routes from './routes';
+const GlobalStyle = lazy(() => import('./styles/GlobalStyles'));
+const Header = lazy(() => import('./components/Header'));
+const Routes = lazy(() => import('./routes'));
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router history={history}>
-          <Header />
-          <Routes />
-          <GlobalStyle />
-          {/* TODO: alterar para 3 ou 4 segundos */}
-          <ToastContainer
-            autoClose={4000}
-            position="top-center"
-            className="toast-container"
-          />
-        </Router>
-      </Provider>
+        <Suspense fallback={<div style={{height: '100vh'}}><div className="d-flex h-100 justify-content-center align-items-center"><h3>Carregando...</h3></div></div>}>
+            <Provider store={store}>
+                <Router history={history}>
+                    <Header />
+                    <Routes />
+                    <GlobalStyle />
+                    {/* TODO: alterar para 3 ou 4 segundos */}
+                    <ToastContainer
+                    autoClose={4000}
+                    position="top-center"
+                    className="toast-container"
+                    />
+                </Router>
+            </Provider>
+        </Suspense>
+
     );
   }
 }
